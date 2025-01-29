eae4c17d8beb669fd01aca273dd241c092d566c1
Aitor Guinea <aguinea@cbr9s1.42barcelona.com>
2025-01-29 11:46:09 +0100

    [W O R K S]

--- mandatory/src/chunks_sort_min_top_utils.c
+++ mandatory/src/chunks_sort_min_top_utils.c
@@ -6,7 +6,7 @@
 /*   By: aguinea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 19:32:34 by aguinea           #+#    #+#             */
-/*   Updated: 2025/01/22 19:42:16 by aguinea          ###   ########.fr       */
+/*   Updated: 2025/01/29 11:36:25 by aguinea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,16 @@ static void	min_top_4(t_stack **a, t_stack **b)
 void	min_top_4_part2(t_stack **a, t_stack **b)
 {
 	pa(a, b, 2);
-	rrb(b, 2);
+	rb(b, 2);
 	if ((*b)->order == 4)
 		pa(a, b, 2);
 	else
 	{
-		rrb(b, 2);
+		rb(b, 2);
 		pa(a, b, 2);
+		rrb(b, 2);
 	}
+	rrb(b, 2);
 	ft_check_swap(a, b);
 	pa(a, b, 2);
 	ft_check_swap(a, NULL);
@@ -54,8 +56,9 @@ void	min_is_top_else(t_stack **a, t_stack **b)
 		pa(a, b, 2);
 		ft_check_swap(a, b);
 		pa(a, b, 2);
-		ft_check_swap(a, b);
+		ft_check_swap(a, NULL);
 		pa(a, b, 2);
+		ft_check_swap(a, NULL);
 	}
 	else
 		min_top_4(a, b);
